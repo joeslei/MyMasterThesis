fb70664515a081f1fd5172a1d09860ed6f39574d
TheAtlasEngine <blackurocky@gmail.com>
2018-08-30 12:58:07 +0900

    HistogramOfEigenValues.py: change the number of bins from 50 to 100

--- NumericalAnalysis/pythonFiles/HistogramOfEigenValues.py
+++ NumericalAnalysis/pythonFiles/HistogramOfEigenValues.py
@@ -5,7 +5,7 @@ import matplotlib.pyplot as plt
 
 def createHistogram():
     numberOfParticles = int(input("input the number of particles: "))
-    numberOfbins = 50
+    numberOfbins = 100
 
     data = diagonalizeHamiltonian(numberOfParticles)["eigenValue"]
 
