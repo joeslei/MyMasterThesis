f1fa064c7b311e762c088281a7df5fb9a4d456b9
TheAtlasEngine <blackurocky@gmail.com>
2018-09-07 18:50:08 +0900

    give up today: september 7th

--- NumericalAnalysis/pythonFiles/billiard_chaos/billiardChaos.py
+++ NumericalAnalysis/pythonFiles/billiard_chaos/billiardChaos.py
@@ -1,2 +1,24 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
 def ellasticCollision(theta):
     return -theta
+
+
+# the radius of the semi-circles
+radius = 1
+
+# the length of a side of a  rectangle
+l = 1
+
+# the speed of a particle
+speed = 1
+
+def borderOfStadium(x, y):
+    circle = np.sqrt(radius ** 2 - (x - l) ** 2)
+    if np.abs(x) <= l:
+        return r if y > 0 else -r
+    else:
+        return circle if  y > 0 else -circle
+
+
