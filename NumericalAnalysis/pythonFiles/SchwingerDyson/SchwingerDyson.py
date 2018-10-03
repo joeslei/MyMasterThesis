42af5128a843858bac3cac7ffe0e1b7a936ad7b6
TheAtlasEngine <blackurocky@gmail.com>
2018-10-03 09:56:10 +0900

    refactoring: SchwingerDyson.py

--- NumericalAnalysis/pythonFiles/SchwingerDyson/SchwingerDyson.py
+++ NumericalAnalysis/pythonFiles/SchwingerDyson/SchwingerDyson.py
@@ -1,7 +1,29 @@
 import numpy as np
 import matplotlib.pyplot as plt
-from scipy.fftpack import dst as DiscreteSineTransform
+from scipy.fftpack import dst
 from sgnFunction import sgn
+
+
+def discreteSineTransform(array):
+    result = dst(array, type=2)
+
+    return result
+
+
+def inverseDiscreteSineTransform(array):
+    # normalization factor
+    f = np.sqrt(1.0 / (2 * len(array)))
+    g = np.sqrt(1.0 / (4 * len(array)))
+
+    result = dst(array, type=3)
+    result = [result[0] * g * g] + [k * f * f for k in result[1:]]
+
+    return result
+
+
+def isConvergentEnough(previousArray, nextArray, epsilon=0.0001):
+    array = [abs(nextArray[n] - previousArray[n]) ** 2 for n in range(len(previousArray))]
+    return sum(array) < epsilon
 
 
 # ---------------------------------
@@ -23,13 +45,6 @@ x = 1.0 / 2
 # the real space__
 theta = [np.pi * k / numOfParticles for k in range(numOfParticles + 1)]
 
-# The number of times of iteration loop
-iterationLoop = 100
-
-# normalization factor
-f = np.sqrt(1.0 / (2 * len(theta)))
-g = np.sqrt(1.0 / (4 * len(theta)))
-
 
 # -------------------------------------------
 # Now let's iterate the Schwinger Dyson eq.
@@ -38,35 +53,34 @@ g = np.sqrt(1.0 / (4 * len(theta)))
 # The initial two point function is the free one.
 twoPointFunction = [0.5 * k for k in sgn(theta)]
 object = [1j * k for k in twoPointFunction]
-transformedTwoPointFunction = DiscreteSineTransform(object, type=3)
-transformedTwoPointFunction = [transformedTwoPointFunction[0] * g * g] + [k * f * f for k in transformedTwoPointFunction[1:]]
+transformedTwoPointFunction = inverseDiscreteSineTransform(object)
 
-for iteration in range(iterationLoop):
-    progress = float(iteration) / iterationLoop * 100
-    if progress % 10 == 0.0:
-        print("{}% done".format(progress))
+cnt = 0
+
+while True:
+    previousTwoPointFunction = twoPointFunction[:]
 
     selfEnergy = [J * J * (k ** (q - 1)) for k in twoPointFunction]
     object = [1j * k for k in selfEnergy]
-    transformedSelfEnergy = DiscreteSineTransform(object, type=3)
-    transformedSelfEnergy = [transformedSelfEnergy[0] * g * g] + [k * f * f for k in transformedSelfEnergy[1:]]
+    transformedSelfEnergy = inverseDiscreteSineTransform(object)
 
     nextTransformedTwoPointFunction = []
     for n in range(len(transformedSelfEnergy)):
         omega = 2 * np.pi * (n + 0.5)
-        # omega = n + 0.5
         term1 = x / (- 1j * omega - transformedSelfEnergy[n])
         term2 = (1 - x) * transformedTwoPointFunction[n]
         nextTransformedTwoPointFunction.append(term1 + term2)
 
     transformedTwoPointFunction = nextTransformedTwoPointFunction[:]
     object = [-1j * k for k in transformedTwoPointFunction]
-    twoPointFunction = DiscreteSineTransform(object, type=2)
-    # twoPointFunction = [twoPointFunction[0] / g] + [k / f for k in twoPointFunction[1:]]
+    twoPointFunction = discreteSineTransform(object)
 
+    if isConvergentEnough(previousTwoPointFunction, twoPointFunction):
+        print("The two point function was convergent enough at cnt = {}".format(cnt))
+        break
 
-# plt.plot(theta, [k.real for k in twoPointFunction], 'b')
-# plt.plot(theta, [k.imag for k in twoPointFunction], 'g')
+    cnt += 1
+
 x = [np.pi * k / numOfParticles for k in range(2 * numOfParticles + 1)]
 y = list(twoPointFunction[:-1:]) + list(twoPointFunction[-1::-1])
 plt.grid(True)
